b06247687669ac702b59c76b4310e6f4f9f58568
Paul <Paul@PaulTreitel>
2018-11-30 23:07:27 -0800

    fixing graphical issues (hopefully)

--- src/Display.java
+++ src/Display.java
@@ -1,109 +1,188 @@
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
+import javax.swing.JComponent;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+/* 
+import javax.swing.JSlider;
+
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+ */
+
+import java.awt.Color;
+import java.awt.Font;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+//import java.awt.geom.Rectangle2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+import javax.swing.ImageIcon;
 
+import java.util.*;
 
+/*
+ * 
+ * 
+ *   We will keep things NOT too flexible here,
+ *   so as to make the design and code simpler.
+ * 
+ *  Things will be in fixed-location on screen.
+ *  This is called Absolute-positioning (as compared to using layouts)
+ *  http://docs.oracle.com/javase/tutorial/uiswing/layout/none.html
+ *  All the definition are grouped below
+ *  The graphic tries to follow the look of:
+ *  http://www.bitstorm.org/gameoflife/ 
+ * 
+ *   Display is responsible for drawing the game.
+ *   Display :
+ *   	Creates and manages interactions with the game object
+ *      Creates and manages the items display (getting the Frame from main)
+ *      Handles all non-button events. That means, getting mouse clicks, 
+ *      determining which cell they hit, and managing response.
+ * 
+ * 
+ */
+//@SuppressWarnings("serial")
+public class Display extends JComponent {
 
-public class Display extends JComponent{
-    
     private int frameWidth, frameHeight;
+    private boolean loopMode;
+
+    private final static long STEP_TIME_MILLIS = 150; // time in milliseconds
+
+    // We can define this in one place as game-rules, and use it here
+    // and in game.java. But let's just keep things a little bit more
+    // compact.
+    private final static int ROWS = 10;
+    private final static int COLS = 10;
+
+    private final JButton resetButton = new JButton();
+    private final JButton stepButton = new JButton();
+    private final JButton runButton = new JButton();
+    private final JButton avgButton = new JButton();
+    private final JLabel avgLabel = new JLabel();
+
+    private final JLabel stepsLabel = new JLabel();
+
+    // Unlike my usual habit, i pushed many of the constants defs to the very bottom.
+    // Too many graphic stuff...
+    private final static int CELL_SIDE_PIXELS = 50;
+    private final static int CELL_TOP_X = 50;
+    private final static int CELL_TOP_Y = 50;
+
+    private final static Color COLOR_EMPTY = Color.CYAN;
+    private final static Color COLOR_GRID = Color.WHITE;
+    private final static Color COLOR_VALUE = Color.GREEN;
+    private final static Color COLOR_MINE = Color.RED;
+    private final static Color COLOR_FONT_VAL = Color.WHITE;
+
+
+    static private ArrayList<JLabel> mineLabels = new ArrayList<>();
+
+    // Graphics locations
+    // Should be as a function of frameWidth and frameHeight
+    // and created in the constructor.
+    // Let's save trouble for now and just put these based on:
+    // (Width, Height) = (600,800)
+    private final static Rectangle RESET_RECT = new Rectangle(50, 550, 50, 40);
+    private final static Rectangle STEP_RECT = new Rectangle(50, 600, 50, 40);
+    private final static Rectangle RUN_RECT = new Rectangle(130, 600, 50, 40);
+    private final static Rectangle AVG_RECT = new Rectangle(210, 600, 100, 40);
+    private final static Rectangle AVG_LABEL_RECT = new Rectangle(320, 600, 40, 40);
+    private final static Rectangle STEPS_RECT = new Rectangle(500, 600, 40, 40);
+
+    
+    String imageFileName = "Mine50x50.png";
+    private ImageIcon image = new ImageIcon(imageFileName);
+    
     
     private Game game;
-    
-    private boolean loopMode;
-    
-    public Display(int w, int h){
-        frameWidth = w;
-        frameHeight = h;
-        
+
+    public Display(int frameWidth, int frameHeight) {
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+
         putButtons();
         init();
     }
-    
-    
+
     private void init() {
-        System.out.println("In init");
-        // new game
-        game = new Game();
         loopMode = false;
-    }
-    
-    public void paintComponent(Graphics g){
-        Graphics2D g2 = (Graphics2D) g;
+        System.out.println("in init: size = "+ mineLabels.size());
+        for (int ii=0; ii<mineLabels.size(); ++ii) {
+            remove(mineLabels.remove(0));
+            validate();
+            repaint();
+        }
+        game = new Game();
         
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
         drawCells(g2);
         drawGrid(g2);
-        drawButtons(g2);
-        
-    }
-    
-    
-    
-    
-    final int CELL_TOP_X = 0;
-    final int CELL_TOP_Y = 0;
-    final int CELL_SIDE_PIXELS = 50;
-    final int COLS = 10;
-    final int ROWS = 10;
-    
-    final Color COLOR_GRID = Color.WHITE;
-    final Color COLOR_EMPTY = Color.BLUE;
-    final Color COLOR_MINE = Color.RED;
-    final Color COLOR_VALUE = Color.GREEN;
-    
+        drawButtons();
 
-    
-    private void drawGrid(Graphics2D g2){
-        
+        if (loopMode) {
+            try {
+                Thread.sleep(STEP_TIME_MILLIS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            boolean done = game.play();
+            if (done) loopMode = false;
+            repaint();
+        }
+
+    }
+
+    private void drawGrid(Graphics2D g2) {
         g2.setColor(COLOR_GRID);
-        
-        // horizontal lines
+
         int x1 = CELL_TOP_X;
-        int x2 = x1 + COLS * CELL_SIDE_PIXELS;
+        int x2 = CELL_TOP_X + COLS * CELL_SIDE_PIXELS;
         int y1, y2;
-    
-        
-        for (int rr=0; rr<=ROWS ; ++rr){
-            y1 = CELL_TOP_Y + rr*CELL_SIDE_PIXELS;
+
+        for (int ii = 0; ii <= ROWS; ii++) {
+            y1 = CELL_TOP_Y + ii * CELL_SIDE_PIXELS;
             g2.drawLine(x1, y1, x2, y1);
         }
-        
-        // vertical lines
+
         y1 = CELL_TOP_Y;
-        y2 = y1 + ROWS * CELL_SIDE_PIXELS;    
-        
-        for (int cc=0; cc<=COLS ; ++cc){
-            x1 = CELL_TOP_X + cc*CELL_SIDE_PIXELS;
+        y2 = CELL_TOP_Y + ROWS * CELL_SIDE_PIXELS;
+        for (int jj = 0; jj <= COLS; jj++) {
+            x1 = CELL_TOP_X + jj * CELL_SIDE_PIXELS;
             g2.drawLine(x1, y1, x1, y2);
         }
-        
-        
-        
+
     }
-    
-    
+
     private void drawCells(Graphics2D g2) {
-        for (int rr=0 ; rr<ROWS ; ++rr){
-            int ytop = CELL_TOP_Y + rr*CELL_SIDE_PIXELS;
+
+        for (int ii = 0; ii < ROWS; ii++) {
+            int ytop = CELL_TOP_Y + ii * CELL_SIDE_PIXELS;
             int ybot = ytop + CELL_SIDE_PIXELS;
-            
-            for(int cc=0; cc<COLS ; ++cc){
-                int xleft = CELL_TOP_X + cc*CELL_SIDE_PIXELS;
+
+            for (int jj = 0; jj < COLS; jj++) {
+
+                int xleft = CELL_TOP_X + jj * CELL_SIDE_PIXELS;
                 int xright = xleft + CELL_SIDE_PIXELS;
-                
-                
+
                 Color c;
-              
-                // ToDO: Call game
-                int val = game.getCell(rr,cc);
+                
+                int fontSize = 40;
+                g2.setFont(new Font("TimesRoman", Font.PLAIN, fontSize));
+
+                int val = game.getCell(ii,jj);
                 switch (val){
                     case 8:
                     case 7:
@@ -117,15 +196,22 @@ public class Display extends JComponent{
                         c = COLOR_VALUE;
                         g2.setColor(c);
                         g2.fillRect(xleft, ytop, CELL_SIDE_PIXELS, CELL_SIDE_PIXELS);
-                        g2.setColor(Color.WHITE);
-                        g2.drawString(""+val,xleft+10,ytop+10);
-                        
+                        g2.setColor(COLOR_FONT_VAL);
+                        int dx = CELL_SIDE_PIXELS/4 ;
+                        int dy = 3*CELL_SIDE_PIXELS/4 ;
+                        g2.drawString("" + val, xleft+dx, ytop+dy);
                         break;
-                        
                     case -1:
+                        // MINE
                         c = COLOR_MINE;
                         g2.setColor(c);
                         g2.fillRect(xleft, ytop, CELL_SIDE_PIXELS, CELL_SIDE_PIXELS);
+                        JLabel label = new JLabel();
+                        label.setBounds(xleft + 1, ytop + 1, CELL_SIDE_PIXELS - 2, CELL_SIDE_PIXELS - 2);
+                        label.setIcon(image);
+                        add(label);
+                        mineLabels.add(label);
+                        //System.out.println("in adding label mine: size = "+ mineLabels.size());
                         break;
                     case -2:
                     default:
@@ -134,116 +220,99 @@ public class Display extends JComponent{
                         g2.fillRect(xleft, ytop, CELL_SIDE_PIXELS, CELL_SIDE_PIXELS);
                         break;
                 }
-                
-            
             }
-            
         }
-        
     }
-    
-    private void drawButtons(Graphics2D g2) {
-    
+
+    private void drawButtons() {
+        stepsLabel.setText(Integer.toString(game.getTurns()));
     }
-    
-    
-    private final JButton resetButton = new JButton();
-    private final JButton stepButton = new JButton();
-    private final JButton runButton = new JButton();
-    private final JButton avgButton = new JButton();
-    
-    private final Rectangle RESET_RECT = new Rectangle(50,500,50,40);
-    private final Rectangle STEP_RECT = new Rectangle(50,530,50,40);
-    private final Rectangle RUN_RECT = new Rectangle(130,530,50,40);
-    private final Rectangle AVG_RECT = new Rectangle(210,530,100,40);
-    private final Rectangle AVG_LABEL_RECT = new Rectangle(320,530,100,40);
-    
-    
-    private JLabel avgLabel = new JLabel();
-    private JLabel stepsLabel = new JLabel();
+
     
     private void putButtons() {
-       
-       resetButton.setText("Reset");
-       resetButton.setBounds(RESET_RECT);
-       class ResetListener implements ActionListener {
-           public void actionPerformed(ActionEvent e) {
-               init();
-               repaint();
-           }
-       }
-       resetButton.addActionListener(new ResetListener());
-       resetButton.setVisible(true);
-       add(resetButton);  // uses the Jcomponent add function
+
+        resetButton.setText("Reset");
+        resetButton.setBounds(RESET_RECT);
+        class ResetListener implements ActionListener {
+
+            public void actionPerformed(ActionEvent e) {
+                init();
+                repaint();
+            }
+        }
+        resetButton.addActionListener(new ResetListener());
+        resetButton.setVisible(true);
+        add(resetButton);
+
         
- 
-       
-       stepButton.setText("Step");
-       stepButton.setBounds(STEP_RECT);
-       class StepListener implements ActionListener {
-           public void actionPerformed(ActionEvent e) {
-               loopMode = false;
-               game.play();
-               repaint();
-           }
-       }
-       stepButton.addActionListener(new StepListener());
-       stepButton.setVisible(true);
-       add(stepButton);  // uses the Jcomponent add function
- 
+        
+        stepButton.setText("Step");
+        stepButton.setBounds(STEP_RECT);
+        class StepListener implements ActionListener {
 
-       runButton.setText("Run");
-       runButton.setBounds(RUN_RECT);
-       class RunListener implements ActionListener {
-           public void actionPerformed(ActionEvent e) {
-               
-               
-               
-               repaint();
-           }
-       }
-       runButton.addActionListener(new RunListener());
-       runButton.setVisible(true);
-       add(runButton);  // uses the Jcomponent add function
- 
-       
-       
-       
-       avgLabel.setBounds(AVG_LABEL_RECT);
-       avgLabel.setText("---");
-       avgLabel.setVisible(true);
-       add(avgLabel);
-       
-       
-       avgButton.setText("AVG");
-       avgButton.setBounds(AVG_RECT);
-       class AvgListener implements ActionListener {
-           public void actionPerformed(ActionEvent e) {
-               final int NUM_OF_AVGS = 1000;
-               double sumTurns = 0;
-               
-               for (int ii=0; ii<NUM_OF_AVGS ; ++ii){
-                   Game gg = new Game();
-                   while (!gg.play() );
-                   sumTurns += gg.getTurns();
-               }
-               
-               System.out.println("sumTurns=" + sumTurns);
-               avgLabel.setText(Integer.toString((int)(sumTurns/NUM_OF_AVGS)));
-               
-               repaint();
-           }
-       }
-       avgButton.addActionListener(new AvgListener());
-       avgButton.setVisible(true);
-       add(avgButton);  // uses the Jcomponent add function
- 
-       
+            public void actionPerformed(ActionEvent e) {
+                loopMode = false;
+                game.play();
+                repaint();
+            }
+        }
+        stepButton.addActionListener(new StepListener());
+        stepButton.setVisible(true);
+        add(stepButton);
+
+        runButton.setText("Run");
+        runButton.setBounds(RUN_RECT);
+        class RunListener implements ActionListener {
+
+            public void actionPerformed(ActionEvent e) {
+                loopMode = !loopMode;
+                String str = (loopMode) ? "Stop" : "Run";
+                runButton.setText(str);
+                repaint();
+            }
+        }
+        runButton.addActionListener(new RunListener());
+        runButton.setVisible(true);
+        add(runButton);
+
+        avgButton.setText("Calc. Avg.");
+        avgButton.setBounds(AVG_RECT);
+
+        avgLabel.setBounds(AVG_LABEL_RECT);
+        avgLabel.setText("--");
+        avgLabel.setVisible(true);
+        add(avgLabel);
+
+        class AvgListener implements ActionListener {
+
+            public void actionPerformed(ActionEvent e) {
+                final int NUM_OF_ITERATIONS = 1000;
+                double sumTurns = 0;
+
+                for (int ii = 0; ii < NUM_OF_ITERATIONS; ++ii) {
+                    Game gg = new Game();
+                    while(! gg.play());
+                    sumTurns += gg.getTurns();
+                }
+
+                System.out.println("sumTurns=" + sumTurns);
+                avgLabel.setText( Integer.toString((int)(sumTurns / NUM_OF_ITERATIONS)) );
+
+                repaint();
+            }
+        }
+        
+        avgButton.addActionListener(new AvgListener());
+        avgButton.setVisible(true);
+        add(avgButton);
 
 
-       
+        //stepLabel.setText(Integer.toString(g.getStepCounter()));
+        stepsLabel.setBounds(STEPS_RECT);
+        stepsLabel.setText("0");
+        stepsLabel.setVisible(true);
+        add(stepsLabel);
+
     }
-    
-    
-    
+
 }
--- src/Game.java
+++ src/Game.java
@@ -1,146 +1,229 @@
 
+import java.util.Random;
 import java.util.Arrays;
 import java.util.Collections;
 
-
 public class Game {
     
+    static Random rand = new Random() ;
+        
+    // See comment in Display
     static final int ROWS = 10;
     static final int COLS = 10;
     
-    static final int MINES = 20;
+    // 
+    static final int MINES = 20 ;
     
-    private int[][] hiddenBoard;
-    private int[][] gameBoard;
+    // board can be a class by itself etc.
+    // BUT, we'll keep it VERY simple with two 2D arrays.    
+    // one for the board itself, and one to represent hits/tries
+    private int[][]     hiddenBoard ;
+    private int[][]     gameBoard ;
     
-    // On the hidden board:
-    // Any number between 0 and 8 denotes the neighbours.
-    //  -1 denotes a Mine.
-    //  -2 not marked (not known)
+    
+    // I do not want to use enum, as want to allow simple values/numbers
+    // There are more fancy ways to do it (enum and calling functions).
+    // BUT, would be MUCH easier to get rolling like this.
     static final int MINE = -1;
-    static final int NOT_MARKED = -2;
+    static final int NOT_MARKED = -2;    
     
-    
-    private int numOfTurns;
     private Player p;
+    private int numOfTurns;
     
-    
-    
-    public Game(){
-        hiddenBoard = new int[ROWS][COLS];
-        gameBoard = new int[ROWS][COLS];
-        
-        p = new Player(ROWS, COLS, MINES);
+    // one variable with various possibilities, or multiple variables
+    private boolean steppedOnMine;
+    private boolean markedWrongMine;
+
+    public Game()
+    {
         numOfTurns = 0;
+        steppedOnMine = false;
+        hiddenBoard = new int[ROWS][COLS];
+        gameBoard   = new int[ROWS][COLS];
+
+        p = new Player(ROWS, COLS);        
         
-        for (int rr=0; rr<ROWS; ++rr)
-            for (int cc=0; cc<COLS; ++cc)
-                gameBoard[rr][cc] = NOT_MARKED;
-                
+        // We will populateBoard AFTER the first click
+        // gameBoard can be done here
+        for (int ii=0; ii<ROWS;++ii)
+            for(int jj=0; jj<COLS ; ++jj)
+                gameBoard[ii][jj] = NOT_MARKED;
+
+        System.out.println("gameBoard Initialized");
+        //printBoard(gameBoard);
+
+        
     }
     
-    public int getCell(int r, int c){
+    public int getCell(int r, int c) {
         return gameBoard[r][c];
         //return hiddenBoard[r][c];
     }
     
-    
     public boolean play() {
 
-        boolean steppedOnMine = false;
-        boolean markedWrongMine = false;
-
-        ++numOfTurns;
+        boolean doneBoolean = false;
+        
+        numOfTurns++;
         p.planNextMove(gameBoard);
         int r = p.getMoveR();
         int c = p.getMoveC();
-        int action = p.getMoveAction(); // -1-Mine, +1-safe
-
-        System.out.println("" + numOfTurns + " :: Move guessed: (r"
-                + r + "," + c + ") --> action=" + action);
+        int action = p.getMoveAction(); // -1-Mine, +1-Safe
+        System.out.println("" + numOfTurns + "::  Move guessed (" + r + "," + c + ") --> Action = " + action);
 
         if (numOfTurns == 1) {
             populateBoard(r, c);
         }
 
-        // action==1 safe
         if (action == 1 && hiddenBoard[r][c] == MINE) {
             steppedOnMine = true;
         }
+
+        // unique for our AI game
         if (action == -1 && hiddenBoard[r][c] != MINE) {
             markedWrongMine = true;
         }
 
-        // If all is Kosher, just mark here:
+        // All Kosher if we got here
         gameBoard[r][c] = hiddenBoard[r][c];
 
+        /* Let's skip this!
+
+            // Just need to expose as relevant
+            // This will give incentive to the AI to find '0's.
+            boolean madeChange = true;
+            
+            // No need to work if this was a mine, or if the Value is not 0.
+            if ( action == -1 || gameBoard[r][c]!=0) madeChange = false;
+            while (madeChange){
+                madeChange = false;
+                for (int rr=0; rr<ROWS; ++rr){
+                    for (int cc=0; cc<COLS; ++cc){
+            
+            QQQQ Need to correct here.
+                        int val = 0;
+                        for (int ii=-1; ii<=1; ii++){
+                            if (rr+ii<0 ||  rr+ii>=ROWS) continue;
+                            for (int ll=-1; ll<=1; ll++){
+                                if (cc+ll<0 ||  cc+ll>=COLS) continue;
+                                if (ii==0 && ll==0) continue;
+                                val +=  (hiddenBoard[rr+ii][cc+ll]==MINE) ? 1 : 0 ;
+                            }
+                        }
+     
+                    }
+                }
+            }
+         */
+        //printBoard(gameBoard);
         if (steppedOnMine) {
             // do something special
-            System.out.println("Stepped on Mine. Boom!");
+            System.out.println("Stepped on a mine. You're out!");
+            doneBoolean = true;
         }
         if (markedWrongMine) {
             // do something special
-            System.out.println("Marked wrong Mine. Boom!");
+            System.out.println("Wrong marking of a mine. You're out!");
+            doneBoolean = true;
         }
 
-        return steppedOnMine || markedWrongMine || done();
+        return doneBoolean || done() ;
     }
-
-    public int getTurns(){
+    
+    private boolean done()
+    {
+        // in our mode, you need to clic on ALL!!
+        return numOfTurns == ROWS*COLS ;
+/*
+        for (int rr=0; rr<ROWS; ++rr)                     // fill it with empty
+            for (int cc=0; cc<COLS; ++cc)
+//                if (hiddenBoard[rr][cc] !=EMPTY && gameBoard[rr][cc] !=HIT)
+                    return false;
+        return true;
+*/
+    }
+    
+    public int getTurns()
+    {
         return numOfTurns;
     }
     
-    public boolean done(){
-        return numOfTurns == ROWS*COLS;
-    }
     
-    // Populating the hidden board
-    private void populateBoard(int r, int c){
-
-        int clickedSpot = r*COLS + c;
+    // populating the hidden board
+    private void populateBoard(int rClicked, int cClicked)
+    {
+        /* 
+        Put Mines totally random, as long as it's not on the clicked spot.
+        We'll do it by shuffeling an array, and picking as many locations as we need.
+        Just skipping the clicked position
+        */
         
+        // on why you have to use Integer
+        // https://stackoverflow.com/questions/3981420/why-does-collections-shuffle-fail-for-my-array
+        int clickedSpot = rClicked*COLS + cClicked;
         Integer[] a = new Integer[ROWS*COLS];
-        for (int ii=0; ii<ROWS*COLS; ++ii)
+        for (int ii=0; ii<ROWS*COLS ; ++ii)
             a[ii] = ii;
+        Collections.shuffle(Arrays.asList(a)) ;
         
-        Collections.shuffle(Arrays.asList(a));
-        
-        int ii=0; 
-        int cnt = 0;
-        while (cnt<MINES){
-            int n = a[ii++];
-            if (n==clickedSpot) continue;
-            cnt++;
-            
-            int rr = n/COLS;
-            int cc = n%COLS;
-            hiddenBoard[rr][cc] = MINE;
-
-            for (int kk=-1; kk<=1 ; ++kk){
-                if (rr+kk<0 || rr+kk>=ROWS) continue;
-                for (int ll=-1; ll<=1 ; ++ll){
-                    if (cc+ll<0 || cc+ll>=COLS) continue;
-                    if ( hiddenBoard[rr+kk][cc+ll]!=MINE )
-                        hiddenBoard[rr+kk][cc+ll]++;
-                    
+        for (int ii=0; ii<MINES; ++ii){ 
+            int n = a[ii] ;
+            if (n==clickedSpot)  continue ;
+            int r = n / COLS;
+            int c = n % COLS;
+            hiddenBoard[r][c] = MINE;
+            // Fill in the neighbours-value
+            for (int kk=-1; kk<=1; kk++){
+                if (r+kk<0 ||  r+kk>=ROWS ) continue;
+                for (int ll=-1; ll<=1; ll++){
+                    if (c+ll<0 ||  c+ll>=COLS) continue;
+                    if (kk==0 && ll==0) continue;
+                    if (hiddenBoard[r+kk][c+ll] !=MINE)
+                        hiddenBoard[r+kk][c+ll]++;
                 }
             }
-
-            
+        
+        
         }
         
-        System.out.println("Created board:");
-        printBoard(hiddenBoard);
+        // populate array with the neighbours-count
+        /*
+        we can actually doing it now WITHIN the creation loop
         
+        for (int rr=0; rr<ROWS; ++rr){                     
+            for (int cc=0; cc<COLS; ++cc){
+                if (hiddenBoard[rr][cc]==MINE) continue;
+                int val = 0;
+                for (int ii=-1; ii<=1; ii++){
+                    if (rr+ii<0 ||  rr+ii>=ROWS) continue;
+                    for (int ll=-1; ll<=1; ll++){
+                        if (cc+ll<0 ||  cc+ll>=COLS) continue;
+                        if (ii==0 && ll==0) continue;
+                        val +=  (hiddenBoard[rr+ii][cc+ll]==MINE) ? 1 : 0 ;
+                    }
+                }
+                hiddenBoard[rr][cc] = val;
+            }
+        }
+        */
+        
+        System.out.println("hiddenBoard Initialized");
+        printBoard(hiddenBoard);
+
     }
     
-    private void printBoard(int[][] a){
-        for (int rr=0; rr<a.length ; ++rr){
-            for (int cc=0; cc<a[0].length; ++cc){
-                System.out.printf("%3d",a[rr][cc]);
+    public void printBoard(int[][] a)
+    {
+        System.out.println("Printing board!!");
+        for (int rr=0; rr<ROWS; ++rr)
+        {
+            for (int cc=0; cc<COLS; ++cc)
+            {
+                System.out.printf("%3d ", a[rr][cc]);
             }
             System.out.println();
         }
+        
     }
+    
 }
-
